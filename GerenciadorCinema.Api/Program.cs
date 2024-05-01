c4ef2962038de77864f67063b5f88d36e3c46835
migueljeffersongit <migueljeffersongit@gmail.com>
2024-05-01 05:22:14 -0300

    feat: Add TimesSpan converter json

--- GerenciadorCinema.Api/Program.cs
+++ GerenciadorCinema.Api/Program.cs
@@ -1,3 +1,4 @@
+using GerenciadorCinema.Application.Common;
 using GerenciadorCinema.Infrastructure;
 using GerenciadorCinema.Infrastructure.Database.Contexts;
 using Microsoft.EntityFrameworkCore;
@@ -6,7 +7,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new TimeSpanJsonConverter());
+});
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
---
+++ GerenciadorCinema.Application/Common/TimeSpanJsonConverter.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using System.Xml;
+
+namespace GerenciadorCinema.Application.Common;
+
+public class TimeSpanJsonConverter : System.Text.Json.Serialization.JsonConverter<TimeSpan>
+{
+    public override TimeSpan Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+    {
+        var stringValue = reader.GetString();
+        return XmlConvert.ToTimeSpan(stringValue);
+    }
+
+    public override void Write(Utf8JsonWriter writer, TimeSpan value, JsonSerializerOptions options)
+    {
+        writer.WriteStringValue(XmlConvert.ToString(value));
+    }
+}
